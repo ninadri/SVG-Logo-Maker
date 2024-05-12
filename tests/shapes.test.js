0080f751660b5d54991d7d1a8f96aa4b87463010
ninadri <159666628+ninadri@users.noreply.github.com>
2024-05-11 23:16:23 -0500

    added test

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,40 +1,43 @@
 const { Triangle, Square, Circle } = require("../lib/shapes");
 
 describe("Triangle", () => {
-  test("should render a svg file with a triangle shape", () => {
-    const item = new Triangle();
-    item.setColor("blue");
-    item.setText("ABC");
+  test("should render a svg file with a black triangle shape with SVG text in the color red ", () => {
+    const text = "SVG";
+    const textColor = "red";
+    const shapeColor = "black";
     const expectedSVG = `<svg width="300" height="200">
-                           <polygon points="150, 18 244, 182 56, 182" fill="blue"/>
-                           <text x="150" y="100" text-anchor="middle" fill="blue">ABC</text>
-                         </svg>`;
+              <polygon points="150, 18 244, 182 56, 182" fill="black"/>
+              <text x="150" y="100" text-anchor="middle" fill="red">SVG</text>
+            </svg>`;
+    const item = new Triangle([text, textColor, shapeColor]);
     expect(item.render()).toEqual(expectedSVG);
   });
 });
 
 describe("Square", () => {
-  test("should render a svg file with a square shape", () => {
-    const item = new Square();
-    item.setColor("red");
-    item.setText("123");
+  test("should render a svg file with a pink square shape with HEY text in the color white ", () => {
+    const text = "HEY";
+    const textColor = "white";
+    const shapeColor = "pink";
     const expectedSVG = `<svg width="300" height="200">
-                           <rect width="200" height="100" x="20" y="20" rx="20" ry="20" fill="red"/>
-                           <text x="100" y="60" text-anchor="middle" fill="red">123</text>
-                         </svg>`;
+              <polygon points="150, 18 244, 182 56, 182" fill="pink"/>
+              <text x="150" y="100" text-anchor="middle" fill="white">HEY</text>
+            </svg>`;
+    const item = new Square([text, textColor, shapeColor]);
     expect(item.render()).toEqual(expectedSVG);
   });
 });
 
 describe("Circle", () => {
-  test("should render a svg file with a circle shape", () => {
-    const item = new Circle();
-    item.setColor("green");
-    item.setText("XYZ");
+  test("should render a svg file with a green circle shape with PUP text in the color yellow ", () => {
+    const text = "PUP";
+    const textColor = "yellow";
+    const shapeColor = "green";
     const expectedSVG = `<svg width="300" height="200">
-                           <circle cx="150" cy="100" r="80" fill="green"/>
-                           <text x="150" y="100" text-anchor="middle" fill="green">XYZ</text>
-                         </svg>`;
+              <polygon points="150, 18 244, 182 56, 182" fill="green"/>
+              <text x="150" y="100" text-anchor="middle" fill="yellow">PUP</text>
+            </svg>`;
+    const item = new Circle([text, textColor, shapeColor]);
     expect(item.render()).toEqual(expectedSVG);
   });
 });
